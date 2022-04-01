07299115182691a4b0f3ed0d93b6e3e8e125bd80
frederico_lopes <fmc.lopes@campus.fct.unl.pt>
2022-04-01 20:45:10 +0100

    seems faster

--- PJ1/src/lemming/LemmingsEvolved.java
+++ PJ1/src/lemming/LemmingsEvolved.java
@@ -20,19 +20,15 @@ public class LemmingsEvolved {
 
         // BASE CASE
         // if there is a null sequence, return {0, 0}
-        if (rows == 1 || cols == 1)
+        if (rows <= 1 || cols <= 1)
             return new int[]{0, 0};
 
         int maxNumPoints = 0;
+        int minNumPairs = Integer.MAX_VALUE;
+        boolean foundPair = false;
 
         int[][] matrix = new int[rows][cols];
-
-        int mapSize = rows /2;
-        if(cols < rows)
-            mapSize = cols/2;
-
-        Map<Integer, Integer> pairs = new HashMap<>(mapSize);
-        pairs.put(0, 0);
+        int[][] pairs = new int[rows][cols];
 
         // GENERAL CASE
         for (int i = 1; i < rows; i++) {
@@ -40,30 +36,31 @@ public class LemmingsEvolved {
                 Lemming l1 = firstSeqTmp.get(j - 1);
                 Lemming l2 = secondSeqTmp.get(i - 1);
 
-                int currentNumberPairs = 0;
-
                 if (l1.getTribe() == l2.getTribe()) { // Lemmings from same tribe?
                     matrix[i][j] = matrix[i - 1][j - 1] + (l1.getPower() + l2.getPower());
-                    currentNumberPairs = pairs.get(matrix[i - 1][j - 1]) + 1;
+                    foundPair = true;
+                } else
+                    matrix[i][j] = Math.max(matrix[i - 1][j], matrix[i][j - 1]);
 
-                } else if (matrix[i - 1][j] > matrix[i][j - 1]) {
-                    matrix[i][j] = matrix[i - 1][j];
-                    currentNumberPairs = pairs.get(matrix[i - 1][j]);
-                } else {
-                    matrix[i][j] = matrix[i][j - 1];
-                    currentNumberPairs = pairs.get(matrix[i][j - 1]);
-                }
+                if (matrix[i][j] == matrix[i][j - 1])
+                    pairs[i][j] = pairs[i][j - 1];
+                else if (matrix[i][j] == matrix[i - 1][j])
+                    pairs[i][j] = pairs[i - 1][j];
+                else
+                    pairs[i][j] = pairs[i][j - 1] + 1;
 
-                if (maxNumPoints < matrix[i][j])
+                if (maxNumPoints < matrix[i][j]) {
                     maxNumPoints = Math.max(matrix[i][j], maxNumPoints); // updates max points
-
-                int current = pairs.getOrDefault(matrix[i][j], 0);
-                if (current == 0 || currentNumberPairs < current)
-                    pairs.put(matrix[i][j], currentNumberPairs);
+                    minNumPairs = pairs[i][j];
+                } else if (maxNumPoints == matrix[i][j]) {
+                    minNumPairs = Math.min(pairs[i][j], minNumPairs);
+                }
             }
         }
+        if(!foundPair)
+            minNumPairs = 0;
 
-        return new int[]{maxNumPoints, pairs.getOrDefault(maxNumPoints, 0)};
+        return new int[]{maxNumPoints, minNumPairs};
     }
 
 }
